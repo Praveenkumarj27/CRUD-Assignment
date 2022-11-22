7d9fde025406bfee5d205742160831f65996d2de
praveenkumarj27 <jpraveenkumar263@gmail.com>
2022-11-22 07:00:24 +0530

    initial

---
+++ dbSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+var userSchema = new mongoose.Schema({
+    firstname:{type:'string',required:true},
+    lastname:{type:'string',required:true},
+    middlename:{type:'string'},
+    email:{
+        type:'string',
+        required:true,
+        unique:true,
+        lowercase:true,
+        validate:(value)=>{
+            return validator.isEmail(value)
+        }
+    },
+    dob:{type:'string'},
+    mobile:{type:'string',default:'000-000-0000',unique:true},
+    password:{type:'string'},
+    occupation:{type:'string'},
+    company:{type:'string'},
+
+})
+
+
+const UserDetails = mongoose.model('users',userSchema);
+module.exports={UserDetails}
